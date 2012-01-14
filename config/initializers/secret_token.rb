1686811b8c81b151e6cb51ae86953e9e6d5360b1
Emily Price <price.emily@gmail.com>
2012-01-13 22:46:42 -0500

    Create Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GlitchBlog::Application.config.secret_token = '2c7de45bfc0b84c63965c2ea72abdaa2a9312e309ace8df3d9b639c68df7e8ef07962913a68e50ffab24a860887276f8f797e51ee66eee2cdf69b70353db31ca'
